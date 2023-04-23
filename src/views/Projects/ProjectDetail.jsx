503cf36eccd8ae46d20ac05558f7fbf4b203d4d6
toaingo00 <nqtoai2181@gmail.com>
2023-04-23 07:28:19 +0700

    create sample init

--- src/views/Projects/ProjectDetail.jsx
+++ src/views/Projects/ProjectDetail.jsx
@@ -2,36 +2,77 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import projectClient from "../../clients/projectClient";
 import { useStateContext } from "../../context/ContextProvider";
-import { Avatar, Chip, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  Chip,
+  Drawer,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 import { colors } from "../../utils/constants";
+import { rolesCode } from "../../utils/roles";
+import { Add } from "@mui/icons-material";
 
 export default function ProjectDetail() {
   const { id } = useParams();
   const [project, setProject] = useState({});
-  const { setLoading } = useStateContext();
+  const { user, setLoading } = useStateContext();
+  const [openCreateSampleDrawer, setOpenCreateSampleDrawer] = useState(false);
+  const [sampleTexts, setSampleTexts] = useState([]);
 
   useEffect(() => {
     setLoading(true);
     projectClient
       .getProjectById({ id, withSamples: 1 })
       .then(({ data }) => {
-        console.log(data.project.samples);
+        console.log(data.project);
         setProject(data.project);
+        const textTitles = data.project.textTitles.split(",");
+        setSampleTexts(
+          Array.apply(null, Array(data.project.numberOfTexts)).map((x, i) => ({
+            title: textTitles[i] || "",
+            text: "",
+          }))
+        );
       })
       .finally(() => {
         setLoading(false);
       });
   }, []);
 
+  const handleChangeSampleTest = (index) => (e) => {
+    sampleTexts[index].text = e.target.value;
+    setSampleTexts([...sampleTexts]);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(sampleTexts);
+  };
+
   return (
     <div>
-      <Typography
-        variant="h3"
-        className="overflow-hidden whitespace-nowrap text-ellipsis"
-      >
-        {project.name}
-      </Typography>
-      <hr className="mb-4" />
+      <div className="flex flex-row justify-between items-baseline">
+        <Typography
+          variant="h3"
+          className="overflow-hidden whitespace-nowrap text-ellipsis"
+        >
+          {project.name}
+        </Typography>
+        {[rolesCode.MANAGER].includes(user.role?.toUpperCase()) && (
+          <Button
+            variant="contained"
+            startIcon={<Add />}
+            onClick={() => setOpenCreateSampleDrawer(true)}
+          >
+            Create Sample
+          </Button>
+        )}
+      </div>
+      <hr className="my-4" />
       <div className="flex flex-col gap-2">
         {project.labelSets?.map((item, index) => (
           <div
@@ -60,10 +101,16 @@ export default function ProjectDetail() {
       </div>
       <div className="mt-8 flex flex-row gap-4">
         {project.samples?.map((s) => (
-          <div className="flex flex-row gap-4 px-4 py-2 items-center cursor-pointer border shadow-sm rounded-lg hover:shadow-lg transition">
+          <div
+            key={`sample - ${s.id}`}
+            onClick={() => {
+              console.log(123);
+            }}
+            className="flex flex-row gap-4 px-4 py-2 items-center cursor-pointer border shadow-sm rounded-lg hover:shadow-lg transition"
+          >
             <div>{s.id}</div>
             <Chip
-              clickable
+              className="!cursor-pointer"
               variant="outlined"
               avatar={<Avatar />}
               label={s.numberOfPerformer}
@@ -71,6 +118,64 @@ export default function ProjectDetail() {
           </div>
         ))}
       </div>
+      <Drawer anchor="right" open={openCreateSampleDrawer}>
+        <Box
+          component="form"
+          onSubmit={onSubmit}
+          className="px-10 py-6 h-full "
+          width={500}
+        >
+          <div className="flex justify-between flex-col h-full ">
+            <div>
+              <div className="text-2xl font-black mb-10 ">New Sample</div>
+              <div className="max-h-[600px] overflow-auto">
+                {sampleTexts.map((item, index) => (
+                  <TextField
+                    key={`sampleText ${index}`}
+                    label={item.title}
+                    value={item.text}
+                    onChange={handleChangeSampleTest(index)}
+                    fullWidth
+                    rows={4}
+                    multiline
+                    margin="dense"
+                  />
+                ))}
+                {sampleTexts.map((item, index) => (
+                  <TextField
+                    key={`sampleText ${index}`}
+                    label={item.title}
+                    value={item.text}
+                    onChange={handleChangeSampleTest(index)}
+                    fullWidth
+                    rows={4}
+                    multiline
+                    margin="dense"
+                  />
+                ))}
+              </div>
+            </div>
+            <div className="flex flex-row items-center gap-2">
+              <LoadingButton
+                // loading={loading}
+                variant="contained"
+                // disabled={!selectNewCourse?.id}
+                // onClick={handleSubmit}
+                type="submit"
+              >
+                Create
+              </LoadingButton>
+              <Button
+                variant="outlined"
+                onClick={() => setOpenCreateSampleDrawer(false)}
+                color="error"
+              >
+                Cancel
+              </Button>
+            </div>
+          </div>
+        </Box>
+      </Drawer>
     </div>
   );
 }
